0d7fbc921f9bf08168d44dc88708a2b993e515f4
Saikat Hassan <saikat2day@gmail.com>
2025-12-29 15:42:02 +0600

    dynamic_url is implemented

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
-from tasks.views import show_task
+from tasks.views import show_task , show_specific_tusk
 urlpatterns = [
-    path ("show-task/",show_task)]
+    path ("show-task/",show_task),
+    path ('show_task/ <id>' , show_specific_tusk)
+    ]
--- tasks/views.py
+++ tasks/views.py
@@ -8,3 +8,7 @@ def contact(request):
     return HttpResponse("<h1 style='color: red '> This is Contact page</h1>")
 def show_task (request) :
     return HttpResponse ( " This is Show Tasks")
+def show_specific_tusk(request , id):
+    print("id" , id)
+    print("id type" , type(id) )
+    return HttpResponse ( f" This is Specific Task page {id} ")
