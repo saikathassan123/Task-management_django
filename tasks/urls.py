642d1b5d28d5b5e62cfdcf23479f4a57fb8ff6b5
Saikat Hassan <saikat2day@gmail.com>
2025-12-30 22:53:13 +0600

    all is ok now after jhemela

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from tasks.views import show_task , show_specific_tusk
+from tasks.views import show_task, show_specific_tusk
+
 urlpatterns = [
-    path ("show-task/",show_task),
-    path ('show_task/ <id>' , show_specific_tusk)
-    ]
+    path("show-task/", show_task),
+    path("show-task/<int:id>/", show_specific_tusk),
+]
--- tasks/views.py
+++ tasks/views.py
@@ -3,12 +3,15 @@ from django.shortcuts import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse ("Welcome to the task management system")
+    return HttpResponse("Welcome to the task management system")
+
 def contact(request):
-    return HttpResponse("<h1 style='color: red '> This is Contact page</h1>")
-def show_task (request) :
-    return HttpResponse ( " This is Show Tasks")
-def show_specific_tusk(request , id):
-    print("id" , id)
-    print("id type" , type(id) )
-    return HttpResponse ( f" This is Specific Task page {id} ")
+    return HttpResponse("<h1 style='color: red'>This is Contact page</h1>")
+
+def show_task(request):
+    return HttpResponse("This is Show Tasks")
+
+def show_specific_tusk(request, id):
+    print("id", id)
+    print("id type", type(id))
+    return HttpResponse(f"This is Specific Task page {id}")
